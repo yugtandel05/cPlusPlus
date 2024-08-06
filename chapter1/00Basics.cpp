fefdde5a6785e74ce1f21c596b3554e8d04e12c4
yugtandel05 <yugtandel05@gmail.com>
2024-08-06 18:21:20 +0530

    printing and next line

--- chapter1/00Basics.cpp
+++ chapter1/00Basics.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
 #include<string>
 using namespace std;
-
+/*
+Chapter-1 : Basics
+Topics to be covered:-
+1) Basic Printing, \n and endl waali cheez.
+2) Variables, printing variables, int, float, and [+,-,*,/] of integers.
+3) Variable naming rules.
+4) Comments
+5) Taking input
+6)Modulus Operator
+7) Float to int, int to float
+8) Hierarchy
+9) Char and ASCII
+*/
 
 int main (){
     cout<<"Chapter - 1 : Basics"<<endl;
---
+++ chapter1/01Printing&nextline.cpp
@@ -0,0 +1,10 @@
+#include<iostream>          // header file
+using namespace std;       // In C++, using namespace std; is a directive that tells the compiler to use the standard namespace. This namespace contains common C++ functionality, including input/output operations (cin, cout), string manipulation (string), and much more.
+
+int main(){
+    cout<<"Hello everyone"<<endl;
+    cout<<"I am yug and ";
+    cout<<"I am from Gujarat\n";
+    cout<<"I like to code";
+    return 0;
+}
